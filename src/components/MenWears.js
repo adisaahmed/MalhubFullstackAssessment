5647319caade83e8c29a36f11b158548e615c76a
Ahmed Adisa <adisa.ahmed881@gmail.com>
2022-12-06 14:34:58 +0100

    Setting up

--- src/components/MenWears.js
+++ src/components/MenWears.js
@@ -1,15 +1,14 @@
 import React from "react";
 import { ClothingBanner, ClothingBody, Coupon } from "./MenWearsContent";
 
-
 const MenWears = () => {
-    return (
-        <>
-            <ClothingBanner />
-            <ClothingBody />
-            <Coupon />
-        </>
-    )
-}
+  return (
+    <>
+      <ClothingBanner />
+      <ClothingBody />
+      <Coupon />
+    </>
+  );
+};
 
-export default MenWears
+export default MenWears;
